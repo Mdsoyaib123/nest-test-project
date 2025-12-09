c85b7ca84610316050a48ddd545e65f9548bb1ca
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-12-09 09:27:52 +0600

    add code

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  authService: AuthService;
+
+  constructor(authService: AuthService) {
+    this.authService = authService;
+  }
+  @Post('register')
+  register() {
+    const result = this.authService.registerUser();
+    return result;
+  }
+
+  @Post('login')
+  login() {
+    return { message: 'User logged in successfully' };
+  }
+}
